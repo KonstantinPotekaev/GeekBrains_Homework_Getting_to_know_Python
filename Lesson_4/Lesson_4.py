54a35aef6a4ed82b24830daac340b7e18563a666
Константин Потекаев <kostya.potekaev@gmail.com>
2022-08-28 09:07:14 +0300

    Выполнено задание 3

--- Lesson_4/Lesson_4.py
+++ Lesson_4/Lesson_4.py
@@ -1,4 +1,6 @@
 import math
+import random
+from tkinter import Variable
 
 def task1(n):
     a = []
@@ -16,5 +18,36 @@ def task1(n):
 def task2(a):
     return set(a)
 
+def task3(k):
+    indexes = {"0": "\u2070",
+           "1": "\u00B9",
+           "2": "\u00B2",
+           "3": "\u00B3",
+           "4": "\u2074",
+           "5": "\u2075",
+           "6": "\u2076",
+           "7": "\u2077",
+           "8": "\u2078",
+           "9": "\u2079",
+           "-": "\u207B"
+           }
+    s = ''
+    for i in range(k,-1,-1):
+        a = random.randint(0,100)
+        if (a!=0):
+            if (i == 0):
+                s+=str(a)
+                break
+            s += str(a)+'*x'
+            if (i>1):
+                temp = str(i)
+                for j in temp:
+                    s+=indexes[j]
+            s+='+'
+    return s
+
+
 #print(task1(100))
-print(*task2([1,1,2,3,4,4,5]))
+#print(*task2([1,1,2,3,4,4,5]))
+
+print(task3(10))
