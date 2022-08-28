3f079a5ad195ade5dc815509d59347ab2cb69750
Константин Потекаев <kostya.potekaev@gmail.com>
2022-08-28 08:46:04 +0300

    Выполнено задание 1

--- Lesson_4/Lesson_4.py
+++ Lesson_4/Lesson_4.py
@@ -1,1 +1,16 @@
+import math
 
+def task1(n):
+    a = []
+    i = 2
+    temp = n
+    while i <= n:
+        if n % i == 0:
+            a.append(i)
+            n //= i
+            i = 2
+        else:
+            i += 1
+    return a
+
+print(task1(100))
